7a4f8dd4104af4f59139f27ec4989481224fece1
eliachow <eliacchow@gmail.com>
2023-07-07 10:43:12 -0700

    update confirmation page to render purchase

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,8 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    # @products = Product.select(:id, :name, :price, :quantity).order(created_at: :desc)
+
   end
 
   def show
